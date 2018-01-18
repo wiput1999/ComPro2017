d036d419f66880cd1616721beb7d888833cb77a3
Wiput Pootong <wiput.pootong@gmail.com>
2018-01-18 08:41:17 +0700

    :sparkles: Initial Commit

---
+++ Week 2/Lab2-1.c
@@ -0,0 +1,13 @@
+// Computer Programming Laboratory Week 2
+// Wiput Pootong (60070090)
+
+#include <stdio.h>
+
+int main(void) {
+    // Just main function here!
+
+    // Just a comment nothing here!
+    printf("Hello C Programming");
+
+    return 0;
+}
